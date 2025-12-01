34abdf636256738b8238dd605fda68b6213c1542
sangwon <tjtkddnjs857@naver.com>
2025-12-01 22:13:01 +0900

    Initial commit

---
+++ blackwhitemap_back/src/main/java/com/blackwhitemap/blackwhitemap_back/BlackwhitemapBackApplication.java
@@ -0,0 +1,13 @@
+package com.blackwhitemap.blackwhitemap_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackwhitemapBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackwhitemapBackApplication.class, args);
+    }
+
+}
---
+++ blackwhitemap_back/src/test/java/com/blackwhitemap/blackwhitemap_back/BlackwhitemapBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackwhitemap.blackwhitemap_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackwhitemapBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
