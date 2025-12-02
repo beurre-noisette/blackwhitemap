19adf8e0453ae9f7a01b4558f11bc96beb36fec9
Sangwon Seo <tjtkddnjs857@naver.com>
2025-12-02 09:44:26 +0900

    [Backend] feat: JPA Config 수정

--- blackwhitemap_back/src/main/java/com/blackwhitemap/blackwhitemap_back/config/JpaConfig.java
+++ blackwhitemap_back/src/main/java/com/blackwhitemap/blackwhitemap_back/config/JpaConfig.java
@@ -2,8 +2,10 @@ package com.blackwhitemap.blackwhitemap_back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.blackwhitemap.blackwhitemap_back.infrastructure")
 public class JpaConfig {
 }
