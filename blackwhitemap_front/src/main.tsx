c1776ae59d324ad4b6b4040a1702c43384f17122
Sangwon Seo <tjtkddnjs857@naver.com>
2026-01-16 09:29:38 +0900

    [Frontend] refactor: Vercel Analytics 추가

--- blackwhitemap_front/src/main.tsx
+++ blackwhitemap_front/src/main.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import "./styles/index.css";
 import App from "./App";
 
@@ -8,4 +11,10 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(
+  <>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </>,
+);
